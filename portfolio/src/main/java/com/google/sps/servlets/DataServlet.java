786ed8e2f5168efbfcce10b603e3211d46d4f9e1
Abraham Haros <abrahamharos@gmail.com>
2020-06-29 19:51:18 +0000

    Update returning function of the servlet

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -20,17 +20,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/** Servlet that returns some example content. TODO: modify this file to handle comments data */
-@WebServlet("/data")
-public class DataServlet extends HttpServlet {
-
-  @Override
-  public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Abraham</h1>");
-  }
-}
-
 /**Servlet that return a greeting for web visitors*/
 @WebServlet("/greeting")
 public class DataServlet extends HttpServlet {
