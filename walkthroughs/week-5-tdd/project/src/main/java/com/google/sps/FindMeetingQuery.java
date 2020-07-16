f9fcfd38ee12509ba139bfdf6329b92c322c3762
Abraham Haros <abrahamharos@gmail.com>
2020-07-15 21:22:02 -0700

    Test driven development exercise

--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,86 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
 
 public final class FindMeetingQuery {
+
+  private final int TOTAL_DAY_MINUTES = 60 * 24;
+
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+    ArrayList<TimeRange> availableTimes;
+
+    // Declaration of an array of boolean that indicates the available minutes in a day.
+    Boolean[] availableMinutesAttendees = new Boolean[TOTAL_DAY_MINUTES];
+    Arrays.fill(availableMinutesAttendees, true);
+
+    Boolean[] availableMinutesOptAttendees = new Boolean[TOTAL_DAY_MINUTES];
+    Arrays.fill(availableMinutesOptAttendees, true);
+
+    Collection<String> attendees = request.getAttendees();
+    Collection<String> optionalAttendees = request.getOptionalAttendees();
+
+    for (Event currentEvent: events) {
+      Collection<String> eventAttendees = currentEvent.getAttendees();
+
+      // Verify if requested attendees are in the current event.
+      Boolean isAttendeeAtEvent = !Collections.disjoint(eventAttendees, attendees);
+      Boolean isOptAttendeeAtEvent = !Collections.disjoint(eventAttendees, optionalAttendees);
+
+      int start = currentEvent.getWhen().start();
+      int end = start + currentEvent.getWhen().duration();
+
+      if (isAttendeeAtEvent) {
+        Arrays.fill(availableMinutesAttendees, start, end, false);
+        Arrays.fill(availableMinutesOptAttendees, start, end, false);
+      } else if (isOptAttendeeAtEvent) {
+        Arrays.fill(availableMinutesOptAttendees, start, end, false);
+      }
+    }
+
+    // First, try to include optional attendees.
+    long duration = request.getDuration();
+    availableTimes = computeAvailableTimes(availableMinutesOptAttendees, duration);
+
+    // If there was no time slot available with optional attendees, return time available with mandatory attendees.
+    return availableTimes.size() > 0 ? availableTimes : computeAvailableTimes(availableMinutesAttendees, duration);
+  }
+
+  /**
+   * Function that compute free slots available given request needs.
+   * @param minutesAvailable type{Boolean} array that contains final minute availability of the request
+   * @param duration type{long} duration wanted for a meeting
+   * @return ArrayList<TimeRange> List of time slots that meet the requirements of the query
+   */
+  private ArrayList<TimeRange> computeAvailableTimes(Boolean[] minutesAvailable, long duration) {
+    int durationFreeSlot = 0;
+    ArrayList<TimeRange> timeRanges = new ArrayList<>();
+
+    for (int i = 0; i < minutesAvailable.length; i++) {
+      // Calculate minutes available from a free slot.
+      if (minutesAvailable[i]) {
+        durationFreeSlot++;
+      } else if (durationFreeSlot > 0) {
+        // Create a timerange instance with the start, end and duration of the free slot.
+        TimeRange timeRange = TimeRange.fromStartDuration(i - durationFreeSlot, durationFreeSlot);
+        // Check if that time slot meets request needs.
+        if (timeRange.duration() >= duration) {
+          timeRanges.add(timeRange);
+        }
+
+        durationFreeSlot = 0;
+      }
+    }
+
+    // Calculate the remaining free slot (same as inside the for loop).
+    TimeRange timeRange = TimeRange.fromStartDuration(minutesAvailable.length - durationFreeSlot, durationFreeSlot);
+    if (timeRange.duration() >= duration) {
+      timeRanges.add(timeRange);
+    }
+
+    return timeRanges;
   }
 }
